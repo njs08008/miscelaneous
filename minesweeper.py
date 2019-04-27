1a5066deb404388f5886de761fcbc6cbd45e2db9
Nicholas Stanford <nicholas.stanford01@gmail.com>
2019-04-27 09:56:42 -0400

    Initial commit

---
+++ minesweeper.py
@@ -0,0 +1,58 @@
+from prettytable import PrettyTable
+import random
+
+# Collect user input for board setup
+while True:
+	try:
+		board_width = int(input('Please enter the board width as an integer between 1 and 20. '))
+		if board_width >= 1 and board_width <= 20:
+			break
+		else:
+			print('The board width must be between 1 and 20.')
+	except:
+		print('Please enter an integer.')
+
+while True:
+	try:
+		board_height = int(input('Please enter the board height as an integer between 1 and 20. '))
+		if board_height >= 1 and board_height <= 20:
+			break
+		else:
+			print('The board height must be between 1 and 20.')
+	except:
+		print('Please enter an integer.')
+
+n_tiles = board_width * board_height
+print(n_tiles)
+
+while True:
+	try:
+		n_mines = int(input('Please enter the number of mines as an integer between 1 and %i. ' % n_tiles))
+		if n_mines >= 1 and n_mines <= n_tiles:
+			break
+		else:
+			print('The number of mines must be between 1 and %i' % n_tiles)
+	except:
+			print('Please enter an integer.')
+
+# Create the board
+true_board = [0] * n_tiles
+mines = random.sample(range(n_tiles), n_mines)
+for mine in mines:
+	true_board[mine] = 1
+
+def prettify_board(ugly_board):
+	pretty_board = PrettyTable()
+	pretty_board.header = False
+	pretty_board.hrules = True
+	for row in ugly_board:
+		pretty_board.add_row(row)
+	return pretty_board
+
+shown_board = [[''] * board_width] * board_height
+
+while True:
+	print('Below is your current board.')
+	print(prettify_board(shown_board))
+
+
---
+++ tic_tac_toe.py
@@ -0,0 +1,54 @@
+from prettytable import PrettyTable
+
+board_definition = []
+for i in range(1, 8, 3):
+	board_definition.append(list(range(i, i + 3)))
+
+current_board = [[''] * 3] * 3
+players = ['Player_1', 'Player_2']
+player_symbols = {'Player_1': 'X', 'Player_2': 'O'}
+current_player = 'Player_1'
+valid_moves = set(range(1, 10))
+move_lookup = {1: (0,0), 2: (0,1), 3: (0,2), 4: (1,0), 5: (1,1), 6: (1,2), 7: (2,0), 8: (2,1), 9: (2,2)}
+moves = {'Player_1': set(), 'Player_2': set()}
+used_moves = set()
+winning_sets = [{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}]
+winner = ''
+done = False
+
+def prettify_board(ugly_board):
+	board = PrettyTable()
+	board.header = False
+	board.hrules = True
+	for row in ugly_board:
+		board.add_row(row)
+	return board
+
+print('Welcome to tic-tac-toe!')
+print('Player_1 is \'' + player_symbols['Player_1'] + '\' and Player_2 is \'' + player_symbols['Player_2'] + '\'.')
+print('Below is your board.')
+print(prettify_board(board_definition))
+print('Please take turns by typing the number corresponding to the space you want.')
+
+while not done:
+	move = int(input(current_player + '\'s move: '))
+	while move in used_moves or move not in valid_moves:
+		if move not in valid_moves:
+			move = int(input('That is not a valid move. Please select an integer from 1 to 9. '))
+		elif move in used_moves:
+			move = int(input('That move is already taken. Please make a different move. '))
+	used_moves = used_moves.union({move})
+	moves[current_player] = moves[current_player].union({move})
+	current_board[move_lookup[move][0]][move_lookup[move][1]] = player_symbols[current_player]
+	print('Below is the current board:')
+	print(prettify_board(current_board))
+	for winning_set in winning_sets:
+		if len(moves[current_player].intersection(winning_set)) == 3:
+			winner = current_player
+			print(winner + ' wins!  Thank you for playing!')
+			done = True
+			break
+	current_player = [player for player in players if player != current_player][0]
+	if len(used_moves) == 9 and winner == '':
+		print('It\'s a tie!  Thank you for playing!')
+		done = True
